270168614b7dfd5cbc13bfad7db628d60244849a
shin nagai <shnnagai@gmail.com>
2020-04-26 13:02:38 +0900

    initial commit

---
+++ Main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+var (
+	actorName    string = "shin"
+	companion    string = "sarah"
+	doctorNumber int    = 3
+	season       int    = 11
+)
+
+func main() {
+	i := "hi"
+	n := 1 == 2
+	fmt.Println(i)
+	fmt.Printf("%v, %T", n, n)
+}
