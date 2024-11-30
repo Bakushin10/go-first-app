2d5f2c0531b4b70233438decfa32dff24200bbf1
Shin Nagai <shnnagai@gmail.com>
2024-11-30 12:14:50 -0500

    add channel

---
+++ practice/pointer.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+type person struct {
+	name string
+	age  int
+}
+
+func zeroval(ival int) {
+	ival = 0
+}
+
+func zeroptr(iptr *int) {
+	*iptr = 0
+}
+
+func strchange(s string) {
+	s = "hello"
+}
+
+func main() {
+
+	i := 1
+	zeroval(i)
+	zeroptr(&i)
+	fmt.Println(i)
+
+	// s = "hello"
+	// strchange(s)
+	// fmt.Println(si)
+
+	s := person{name: "Shin", age: 50}
+
+	sp := &s
+
+	s.age = 62
+
+	fmt.Println(sp)
+	fmt.Println(s)
+
+}
